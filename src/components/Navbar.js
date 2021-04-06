2b8e200ce2926cc2765b5bd6178d883c86978031
Ikra Rafi <irafi@nlihc.org>
2021-04-06 18:18:07 -0400

    fontawesome hamburger svg navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "../ikra_Logo.png";
-
+// React Font Awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
     return (
@@ -12,7 +14,7 @@ export const Navbar = () => {
     
   <a className="navbar-brand" href="#"><img className="logo" src ={logo} alt="logo"/></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    <FontAwesomeIcon icon={faBars} style={{color: "#fdc214"}} />
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
