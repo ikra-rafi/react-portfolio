4a7afba5b47ddb2a08a4be2c8ae799f8ef330ac3
Ikra Rafi <irafi@nlihc.org>
2021-04-06 23:54:02 -0400

    about me with photo and bio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import Particles from 'react-particles-js';
+import AboutMe from "./components/AboutMe";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
       />
    <Navbar />
    <Header />
+   <AboutMe />
    </>
   );
 }
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,16 +1,26 @@
 import React from "react";
-import author from "../me.jpg";
+import author from "../me.png";
 
 const AboutMe = () => {
     return (
         <div className="container py-5">
             <div className="row">
             <div className="col-lg-6 col-xm-12">
-                <img src=""{author} alt="ikra" />
+                <div className="photo-wrap mb-4">
+                <img src={author} alt="ikra" className="profile-img" />
+                </div>
             </div>
             <div className="col-lg-6 col-xm-12">
-            <h1>About Me</h1>
-            <p>This is me.</p>
+            <h1 className="about-heading">About Me</h1>
+            <p><strong>Hello! Nice to meet you, I'm Ikra. 
+            </strong></p>
+                <p>
+                I always had a passion for art and design and graduated from 
+                George Mason University with a Bachelor of Fine Arts in Arts and Visual Technology in 2017.
+                I currently work as a Creative Service Coordinator at a nonprofit, National Low Income Housing Coalition, in Washington D.C.
+                The main functions of my responsibilities are cms, print and interactive graphics. Ever since the pandemic, I wanted to explore
+                more options for creativity and found myself enrolling at a certificate program at George Washington University for Full Stack Development. 
+                </p>
             </div>
             </div>
         </div>
