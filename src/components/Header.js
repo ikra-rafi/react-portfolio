876c8f0a3ba6b3b596a636d7c42bc16daae88620
Ikra Rafi <irafi@nlihc.org>
2021-04-06 21:33:19 -0400

    adding hero image

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+       <div className="header-wraper">
+           <div className="main-info">
+               <h1>Welcome to my portfolio</h1>
+           </div>
+
+       </div>
+    )
+}
+
+export default Header;
