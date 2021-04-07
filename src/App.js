087da787da8c29e8012d8947c89cdf897497c3d9
Ikra Rafi <irafi@nlihc.org>
2021-04-07 17:48:42 -0400

    portfolio component start images

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
 import Experience from "./components/Resume";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
    <AboutMe />
    <Services />
    <Experience />
+   <Portfolio />
    </>
   );
 }
---
+++ src/components/Portfolio.js
@@ -0,0 +1,59 @@
+import React from "react";
+import budget from "../images/Budget-Table.png";
+import burger from "../images/burgerapp.png";
+import fitness from "../images/fitness-2.png";
+import foodastic from "../images/foodastic.png";
+import petocracy from "../images/petocracy.png";
+import weather from "../images/weather-app.png";
+import quiz from "../images/wrongquiz.png";
+
+const Portfolio = () => {
+    return (
+        <div className= "portfolio-wrapper">
+            <div className= "container">
+                <h1 className="text-uppercase text-center py">Portfolio</h1>
+                <div className="image-box-wrapper row justify-content-center">
+                    <div className="portfolio-image-box">
+                    <img className="portfolio-image" src={petocracy} alt="Petocracy App" />
+                    <div className="overflow"></div>
+                </div>
+                {/* - */}
+                <div className="portfolio-image-box">
+                    <img className="portfolio-image" src={foodastic} alt="Foodastic App" />
+                    <div className="overflow"></div>
+                </div>
+                {/* - */}
+                <div className="portfolio-image-box">
+                    <img className="portfolio-image" src={quiz} alt="State Capital Quiz App" />
+                    <div className="overflow"></div>
+                </div>
+               
+                {/* - */}
+                
+                <div className="portfolio-image-box">
+                    <img className="portfolio-image" src={budget} alt="Budget Tracking App" />
+                    <div className="overflow"></div>
+                </div>
+                {/* - */}
+                <div className="portfolio-image-box">
+                    <img className="portfolio-image" src={burger} alt="Burger Logger App" />
+                    <div className="overflow"></div>
+                </div>
+                {/* - */}
+                <div className="portfolio-image-box">
+                    <img className="portfolio-image" src={fitness} alt="Fitness Tracking App" />
+                    <div className="overflow"></div>
+                </div>
+                {/* - */}
+                <div className="portfolio-image-box">
+                    <img className="portfolio-image" src={weather} alt="Weather App" />
+                    <div className="overflow"></div>
+                </div>
+                {/* - */}
+                </div>
+            </div>
+            </div>
+    )
+}
+
+export default Portfolio;
