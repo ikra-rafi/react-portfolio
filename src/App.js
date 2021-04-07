6c9de7a1b8f4dc3b9c030389668518fd4485fa3b
Ikra Rafi <irafi@nlihc.org>
2021-04-06 22:50:44 -0400

    particles and media sizes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <>
+    <Particles 
+    className="particles-canvas"
+    params={{
+      particles: {
+      number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 900
+      }
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 2,
+          color: "#edebd9"
+
+      }
+      }}
+      }} 
+      />
    <Navbar />
    <Header />
    </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Typed from "react-typed";
+import Particles from "react-particles-js";
 
 const Header = () => {
     return (
@@ -13,6 +14,7 @@ const Header = () => {
                backSpeed={60}
                loop
                />
+               <a href="#" className="btn-main-offer">Contact Me</a>
            </div>
 
        </div>
