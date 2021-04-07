45600e11800f67ef8b281a5682c3bf1b6cfd1d9d
Ikra Rafi <irafi@nlihc.org>
2021-04-06 21:48:01 -0400

    text effect hero

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Typed from "react-typed";
 
 const Header = () => {
     return (
        <div className="header-wraper">
            <div className="main-info">
                <h1>Welcome to my portfolio</h1>
+               <Typed
+               className="typed-text"
+               strings={["Web Design", "Web Development", "Graphic Design", "Google Analytics"]}
+               typeSpeed={20}
+               backSpeed={60}
+               loop
+               />
            </div>
 
        </div>
